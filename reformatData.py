37fac2508bf13c97df0de8ede563f9a359c3ee3b
aditipate <apatel72@luc.edu>
2021-03-22 17:00:52 -0500

    fixed path

--- reformatData.py
+++ reformatData.py
@@ -2,11 +2,12 @@ import os
 
 
 def renameFastqs(SRRs):
+    current_path = os.getcwd()                                               #change into SRA data folder directory
+    folder = "/mouse_heart_SRA_data"
+    os.mkdir(current_path + folder)
+    os.chdir(current_path + folder)
+
     for i in range(0,len(SRRs)):
-        current_path = os.getcwd()                            #change into SRA data folder directory
-        folder = "/mouse_heart_SRA_data"
-        os.mkdir(current_path + folder)
-        os.chdir(current_path + folder)
 
         #rename fastq files to cellranger compatitable format
         rename_fastq1 = 'mv ' + SRRs[i] + '.1_1.fastq ' + SRRs[i] + '.1' + '_S1_L00' + str(SRRs[i][-1]) + '_R1' + '_001.fastq'
@@ -17,6 +18,9 @@ def renameFastqs(SRRs):
         os.system(rename_fastq1)
         os.system(rename_fastq2)
         os.system(rename_fastq3)
+
+    os.chdir(current_path)                                                     #change to current directory
+
 
 
 

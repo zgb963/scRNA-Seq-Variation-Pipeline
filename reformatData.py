7923749276962a18852ffefbeea0cd810b5978b7
aditipate <apatel72@luc.edu>
2021-04-10 14:36:58 -0500

    renamed fastqs based on original naming convention on SRA

--- reformatData.py
+++ reformatData.py
@@ -6,20 +6,32 @@ def renameFastqs(SRRs):
     folder = "/mouse_heart_SRA_data"
     os.chdir(current_path + folder)
 
+
     for i in range(0,len(SRRs)):
-
         #rename fastq files to cellranger compatitable format
-        rename_fastq1 = 'mv ' + SRRs[i] + '.1_1.fastq ' + SRRs[i] + '.1' + '_S1_L00' + str(SRRs[i][-1]) + '_R1' + '_001.fastq'
-        rename_fastq2 = 'mv ' + SRRs[i] + '.1_2.fastq ' + SRRs[i] + '.1' + '_S1_L00' + str(SRRs[i][-1]) + '_R2' + '_001.fastq'
-        rename_fastq3 = 'mv ' + SRRs[i] + '.1_3.fastq ' + SRRs[i] + '.1' + '_S1_L00' + str(SRRs[i][-1]) + '_I1' + '_001.fastq'
+        if(SRRs[i][-1] == '1'):
+            rename_fastq1 = 'mv ' + SRRs[i] + '.1_1.fastq ' + 'SAN' + '_S' + str(i + 1) + '_L005' + '_R1' + '_001.fastq'
+            rename_fastq2 = 'mv ' + SRRs[i] + '.1_2.fastq ' + 'SAN' + '_S' + str(i + 1) + '_L005' + '_R2' + '_001.fastq'
+            rename_fastq3 = 'mv ' + SRRs[i] + '.1_3.fastq ' + 'SAN' + '_S' + str(i + 1) + '_L005' + '_I1' + '_001.fastq'
 
+        elif(SRRs[i][-1] == '3'):
+            rename_fastq1 = 'mv ' + SRRs[i] + '.1_1.fastq ' + 'AVN' + '_S' + str(i + 1) + '_L005' + '_R1' + '_001.fastq'
+            rename_fastq2 = 'mv ' + SRRs[i] + '.1_2.fastq ' + 'AVN' + '_S' + str(i + 1) + '_L005' + '_R2' + '_001.fastq'
+            rename_fastq3 = 'mv ' + SRRs[i] + '.1_3.fastq ' + 'AVN' + '_S' + str(i + 1) + '_L005' + '_I1' + '_001.fastq'
+
+        elif(SRRs[i][-1] == '5'):
+            rename_fastq1 = 'mv ' + SRRs[i] + '.1_1.fastq ' + 'LPF' + '_S' + str(i + 1) + '_L005' + '_R1' + '_001.fastq'
+            rename_fastq2 = 'mv ' + SRRs[i] + '.1_2.fastq ' + 'LPF' + '_S' + str(i + 1) + '_L005' + '_R2' + '_001.fastq'
+            rename_fastq3 = 'mv ' + SRRs[i] + '.1_3.fastq ' + 'LPF' + '_S' + str(i + 1) + '_L005' + '_I1' + '_001.fastq'
+
+        elif(SRRs[i][-1] == '7'):
+            rename_fastq1 = 'mv ' + SRRs[i] + '.1_1.fastq ' + 'RPF' + '_S' + str(i + 1) + '_L005' + '_R1' + '_001.fastq'
+            rename_fastq2 = 'mv ' + SRRs[i] + '.1_2.fastq ' + 'RPF' + '_S' + str(i + 1) + '_L005' + '_R2' + '_001.fastq'
+            rename_fastq3 = 'mv ' + SRRs[i] + '.1_3.fastq ' + 'RPF' + '_S' + str(i + 1) + '_L005' + '_I1' + '_001.fastq'
 
         os.system(rename_fastq1)
         os.system(rename_fastq2)
         os.system(rename_fastq3)
 
     os.chdir(current_path)                                                     #change to current directory
-
-
-
 

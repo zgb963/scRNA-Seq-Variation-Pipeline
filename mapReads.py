0093e895f72b0ad0897c346ea982261a07f3137c
aditipate <apatel72@luc.edu>
2021-04-11 00:00:07 -0500

    fixed samples

--- mapReads.py
+++ mapReads.py
@@ -5,8 +5,6 @@ def runCellRanger(SRRs):
     id  = 'cellranger_output'                                                    #path designated for cellranger output
     fastqs_path = current_path + '/mouse_heart_SRA_data'                         #path to folder containing fastqs from SRA
     transcriptome = current_path + '/mouse_genome/refdata-gex-mm10-2020-A'       #path to mouse genome
-    samples = ' '.join([str(SRR) + '.1' for SRR in SRRs])
-    samples = samples.replace(' ', ',')
-    cellranger_cmd = 'cellranger count --id=' + id + ' --fastqs=' + fastqs_path + ' --sample=' + samples + ' --transcriptome=' + transcriptome
+    cellranger_cmd = 'cellranger count --id=' + id + ' --fastqs=' + fastqs_path + ' --sample=' + 'SAN,AVN,LPF,RPF' + ' --transcriptome=' + transcriptome
     os.system(cellranger_cmd)
 

35ae24a0f873cc9e8576b8dee1f71c59c89fde5c
aditipate <apatel72@luc.edu>
2021-04-22 23:06:47 -0500

    paths

--- reformatDataGEO.py
+++ reformatDataGEO.py
@@ -8,10 +8,10 @@ def moveGEO():
     os.chdir(current_path + folder)
 
     #prepare folders
-    SAN_folder = '/SAN'
-    AVN_folder = '/AVN'
-    LPF_folder = '/LPF'
-    RPF_folder = '/RPF'
+    SAN_folder = '/SAN_GEO'
+    AVN_folder = '/AVN_GEO'
+    LPF_folder = '/LPF_GEO'
+    RPF_folder = '/RPF_GEO'
 
     #create folders
     os.mkdir(current_path + folder + SAN_folder)
@@ -20,24 +20,21 @@ def moveGEO():
     os.mkdir(current_path + folder + RPF_folder)
 
     #move files into designated folders
-    for filename in os.listdir(current_path + folder):
-        if('GSM3885058' in filename):
-            os.system('mv ' + current_path + folder + '/GSM3885058_SANbarcodes.tsv.gz ' + current_path + folder + SAN_folder)
-            os.system('mv ' + current_path + folder + '/GSM3885058_SANgenes.tsv.gz ' + current_path + folder + SAN_folder)
-            os.system('mv ' + current_path + folder + '/GSM3885058_SANmatrix.mtx.gz ' + current_path + folder + SAN_folder)
-        elif('GSM3885059' in filename):
-            os.system('mv ' + current_path + folder + '/GSM3885059_AVNbarcodes.tsv.gz ' + current_path + folder + AVN_folder)
-            os.system('mv ' + current_path + folder + '/GSM3885059_AVNgenes.tsv.gz ' + current_path + folder + AVN_folder)
-            os.system('mv ' + current_path + folder + '/GSM3885059_AVNmatrix.mtx.gz ' + current_path + folder + AVN_folder)
-        elif('GSM3885060' in filename):
-            os.system('mv ' + current_path + folder + '/GSM3885060_LPFbarcodes.tsv.gz ' + current_path + folder + LPF_folder)
-            os.system('mv ' + current_path + folder + '/GSM3885060_LPFgenes.tsv.gz ' + current_path + folder + LPF_folder)
-            os.system('mv ' + current_path + folder + '/GSM3885060_LPFmatrix.mtx.gz ' + current_path + folder + LPF_folder)
-        elif('GSM3885061' in filename):
-            os.system('mv ' + current_path + folder + '/GSM3885061_RPFbarcodes.tsv.gz ' + current_path + folder + RPF_folder)
-            os.system('mv ' + current_path + folder + '/GSM3885061_RPFgenes.tsv.gz ' + current_path + folder + RPF_folder)
-            os.system('mv ' + current_path + folder + '/GSM3885061_RPFmatrix.mtx.gz ' + current_path + folder + RPF_folder)
-        else: continue
+    os.system('mv ' + current_path + folder + '/GSM3885058_SANbarcodes.tsv.gz ' + current_path + folder + SAN_folder)
+    os.system('mv ' + current_path + folder + '/GSM3885058_SANgenes.tsv.gz ' + current_path + folder + SAN_folder)
+    os.system('mv ' + current_path + folder + '/GSM3885058_SANmatrix.mtx.gz ' + current_path + folder + SAN_folder)
+
+    os.system('mv ' + current_path + folder + '/GSM3885059_AVNbarcodes.tsv.gz ' + current_path + folder + AVN_folder)
+    os.system('mv ' + current_path + folder + '/GSM3885059_AVNgenes.tsv.gz ' + current_path + folder + AVN_folder)
+    os.system('mv ' + current_path + folder + '/GSM3885059_AVNmatrix.mtx.gz ' + current_path + folder + AVN_folder)
+
+    os.system('mv ' + current_path + folder + '/GSM3885060_LPFbarcodes.tsv.gz ' + current_path + folder + LPF_folder)
+    os.system('mv ' + current_path + folder + '/GSM3885060_LPFgenes.tsv.gz ' + current_path + folder + LPF_folder)
+    os.system('mv ' + current_path + folder + '/GSM3885060_LPFmatrix.mtx.gz ' + current_path + folder + LPF_folder)
+
+    os.system('mv ' + current_path + folder + '/GSM3885061_RPFbarcodes.tsv.gz ' + current_path + folder + RPF_folder)
+    os.system('mv ' + current_path + folder + '/GSM3885061_RPFgenes.tsv.gz ' + current_path + folder + RPF_folder)
+    os.system('mv ' + current_path + folder + '/GSM3885061_RPFmatrix.mtx.gz ' + current_path + folder + RPF_folder)
 
 
 
@@ -45,9 +42,6 @@ def moveGEO():
 
 
 
-# shutil.move(os.path.join(current_path + folder, 'GSM3885061_RPFbarcodes.tsv.gz'), current_path + folder + RPF_folder)
-# shutil.move(os.path.join(current_path + folder, 'GSM3885061_RPFgenes.tsv.gz'), current_path + folder + RPF_folder)
-# shutil.move(os.path.join(current_path + folder, 'GSM3885061_RPFmatrix.mtx.gz'), current_path + folder + RPF_folder)
 
 
 

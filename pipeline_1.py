6b634f0496358e780daa8ccd36d6f8b2a7544ef6
Genevieve-Baddoo <genebaddoo@gmail.com>
2022-03-19 21:23:10 -0500

    added comments

--- pipeline_1.py
+++ pipeline_1.py
@@ -1,15 +1,16 @@
-#set up environment
+#set up environment to retrieve, reformat, and map SRA reads
 import retrieveDataSRA
 import reformatDataSRA
 import mapReads
 
-#GEO Accession  -- CCS Component -- SRA run (raw data)
-#GSM3885058     -- Zone I: SAN region -- SRR9290711
+#From NCBI GEO database
+#GEO Accession -- CCS Component -- SRA run (raw data)
+#GSM3885058 -- Zone I: SAN region -- SRR9290711
 #GSM3885059	-- Zone II: AVN/His region -- SRR9290713
 #GSM3885060	-- Zone III (Left): BB/PF region (Left Ventricle) -- SRR9290715
 #GSM3885061	-- Zone III (Right): BB/PF region (Right Ventricle) -- SRR9290717
 
-#list of SRA sample runs (one of each)
+#created list of SRA sample runs (one of each)
 SRRs = ['SRR9290711', 'SRR9290713', 'SRR9290715', 'SRR9290717']
 
 #retrieve mouse heart data from NCBI's SRA + retrieve mouse reference genome

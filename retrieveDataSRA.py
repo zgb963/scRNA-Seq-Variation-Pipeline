81d16aeecbfd7306783e75473b7ced72bad491e5
Genevieve-Baddoo <genebaddoo@gmail.com>
2022-03-22 12:39:23 -0500

    get SRA reads from NCBI

--- retrieveDataSRA.py
+++ retrieveDataSRA.py
@@ -3,7 +3,7 @@ import os
 
 #function to retrieve mouse heart data from NCBI's SRA database
 def getSRAdata(SRRs):
-    current_path = os.getcwd()                                                   #create and change to SRA data folder directory
+    current_path = os.getcwd()   #create and change to SRA data folder directory
     folder = "/mouse_heart_SRA_data"
     os.mkdir(current_path + folder)
     os.chdir(current_path + folder)
@@ -13,23 +13,23 @@ def getSRAdata(SRRs):
             SRR_data_address = 'https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-15/' + SRR + '/' + SRR + '.1'
             wget_SRR = 'wget' + ' ' + SRR_data_address
             fastq_dump_SRR = 'fastq-dump -I --split-files' + ' ' + SRR + '.1'
-            os.system(wget_SRR)                                                #retrieve SRA data using wget command
-            os.system(fastq_dump_SRR)                                          #uncompress data and convert to paired-end fastq files using fastq-dump command
+            os.system(wget_SRR)  #retrieve SRA data using wget command
+            os.system(fastq_dump_SRR) #uncompress data and convert to paired-end fastq files using fastq-dump command
 
 
         else:
             SRR_data_address = 'https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos3/sra-pub-run-20/' + SRR + '/' + SRR + '.1'
             wget_SRR = 'wget' + ' ' + SRR_data_address
             fastq_dump_SRR = 'fastq-dump -I --split-files' + ' ' + SRR + '.1'
-            os.system(wget_SRR)                                                #retrieve SRA data using wget command
-            os.system(fastq_dump_SRR)                                          #uncompress data and convert to paired-end fastq files using fastq-dump command
+            os.system(wget_SRR)   #retrieve SRA data using wget command
+            os.system(fastq_dump_SRR)  #uncompress data and convert to paired-end fastq files using fastq-dump command
 
-    os.chdir(current_path)                                                     #change to current directory
+    os.chdir(current_path) #change to current directory
 
 
 #function to retrieve mouse reference genome index
 def getRefGenome():
-    current_path = os.getcwd()                                                  #create and change to mouse genome folder directory
+    current_path = os.getcwd #create and change to mouse genome folder directory
     folder = "/mouse_genome"
     os.mkdir(current_path + folder)
     os.chdir(current_path + folder)
@@ -39,7 +39,7 @@ def getRefGenome():
     os.system(wget_genome)
     os.system(decompress_genome)
 
-    os.chdir(current_path)                                                       #change to current directory
+    os.chdir(current_path) #change to current directory
 
 
 

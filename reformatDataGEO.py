1b7ffb132f5b26fe868f46388a53607f315d48cc
Genevieve-Baddoo <genebaddoo@gmail.com>
2022-03-22 12:34:10 -0500

    reformat data

--- reformatDataGEO.py
+++ reformatDataGEO.py
@@ -2,17 +2,17 @@ import os
 
 #function to rename GEO barcodes, genes, and matrix files and prepare appropriate folders for Seurat
 def moveGEO():
-    current_path = os.getcwd()                                                          #change into GEO data folder directory
+    current_path = os.getcwd()   #change into GEO data folder directory
     folder = "/mouse_heart_GEO_data"
     os.chdir(current_path + folder)
 
-    #prepare folders
-    SAN_folder = '/SAN_GEO'
-    AVN_folder = '/AVN_GEO'
-    LPF_folder = '/LPF_GEO'
-    RPF_folder = '/RPF_GEO'
+    #prepare folders for 4 zomes of the heart
+    SAN_folder = '/SAN_GEO' #ZONE I
+    AVN_folder = '/AVN_GEO' #ZONE II
+    LPF_folder = '/LPF_GEO' #ZONE III
+    RPF_folder = '/RPF_GEO' #ZONE III
 
-    #create folders
+    #create folders for each zone
     os.mkdir(current_path + folder + SAN_folder)
     os.mkdir(current_path + folder + AVN_folder)
     os.mkdir(current_path + folder + LPF_folder)
@@ -35,9 +35,7 @@ def moveGEO():
     os.system('mv ' + current_path + folder + '/GSM3885061_RPFgenes.tsv.gz ' + current_path + folder + RPF_folder  + '/features.tsv.gz')
     os.system('mv ' + current_path + folder + '/GSM3885061_RPFmatrix.mtx.gz ' + current_path + folder + RPF_folder + '/matrix.mtx.gz')
 
-
-
-    os.chdir(current_path)                                                                #change to current directory
+    os.chir(current_path) #change to current directory
 
 
 
